675bc76cbf0e3bb8e034f38038413ffe9a2eac2c
Sirinyapat <katang27004@hotmail.com>
2020-09-11 10:13:26 +0700

    commit checkpoint2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import CharacterCard from './CharacterCard';
 import React from 'react';
-import './App.css';
 
 function App() {
  return (
-  <div>
-    Hello {"World"}  
-  </div>
+ <div>
+ <CharacterCard value="h"/>
+ <CharacterCard value="i"/>
+ </div>
  );
 }
 export default App;
+
