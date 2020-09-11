e9bb80c400fa62278666ecd6cfb8cdd6d1e90b4f
Sirinyapat <katang27004@hotmail.com>
2020-09-11 11:03:24 +0700

    commit checkpoint3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import CharacterCard from './CharacterCard';
 import React from 'react';
 import WordCard from './WordCard'
-
 const word = "Hello";
 function App() {
   return (
     <div>
       <WordCard value="hello"/>
     </div>
-  );
+    );
+  
 }
 
 export default App;
